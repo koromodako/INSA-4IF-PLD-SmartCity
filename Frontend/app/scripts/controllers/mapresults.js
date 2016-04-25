1d5cbe1a6b4d23b89e54f6dffe20a2a32afc22eb
Rémy de Leydet <remy.deleydet@gmail.com>
2016-04-25 20:03:11 +0200

    Frontend - Mise en place du slider pour la recherche et la carte

---
+++ Frontend/app/scripts/controllers/mapresults.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartCityFrontEndApp.controller:MapResultsCtrl
+ * @description
+ * # MapResultsCtrl
+ * Controller of the smartCityFrontEndApp
+ */
+angular.module('smartCityFrontEndApp')
+  .controller('MapResultsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
