2e44907fedde2efe324f4ababf0450c57dac17b0
Rémy de Leydet <remy.deleydet@gmail.com>
2016-04-25 11:54:02 +0200

    Frontend - Début des animations pour la recherche

--- Frontend/app/scripts/controllers/search.js
+++ Frontend/app/scripts/controllers/search.js
@@ -8,6 +8,16 @@
  * Controller of the smartCityFrontEndApp
  */
 angular.module('smartCityFrontEndApp')
-  .controller('SearchCtrl', function () {
-    
+  .controller('SearchCtrl', function ($scope) {
+    $scope.displayedView = 0;
+    $scope.prev = function() {
+        if ($scope.displayedView > 0) {
+            $scope.displayedView--;
+        }
+    };
+    $scope.next = function() {
+        if ($scope.displayedView < 2) {
+            $scope.displayedView++;
+        }
+    };
   });
