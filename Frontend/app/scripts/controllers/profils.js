b99b0d41b472bce893d766604a7056b58ffc5b2d
Rémy de Leydet <remy.deleydet@gmail.com>
2016-04-26 16:51:52 +0200

    Frontend - Mise en place du stockage des critères après changement de pages

--- Frontend/app/scripts/controllers/profils.js
+++ Frontend/app/scripts/controllers/profils.js
@@ -11,20 +11,26 @@
   .controller('ProfilsCtrl', function ($scope, serviceAjax, searchData) {
     $scope.searchData = searchData;
     $scope.show = false;
-    var loadProfilsAndCriterias = function (){
+    var loadProfils = function (){
       serviceAjax.profils(function(data){
          $scope.profils = data; 
       });
+    };   
+    var loadCriterias = function (){
       serviceAjax.criterias(function(data){
+         searchData.criterias.length = 0;
          for (var i = 0 ; i < data.length ; ++i){
              searchData.criterias.push({name : data[i].name, coef : 5}); 
         }
       });
-    };    
+    };   
              
     $scope.loadDetails = function (){
         $scope.show = true;
     }; 
      
-    loadProfilsAndCriterias();
+    loadProfils();
+    if (searchData.criterias.length === 0){
+        loadCriterias();
+    }
   });
