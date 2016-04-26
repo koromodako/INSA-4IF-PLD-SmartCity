0676cbc1591fea7584f7af69a5897f4a4e373ea8
crihouey <cindy.rihouey@insa-lyon.fr>
2016-04-26 19:48:31 +0200

    changement des coefs en fonction des profils + 2ème slider sur les échelles des critères

--- Frontend/app/scripts/controllers/searchResults.js
+++ Frontend/app/scripts/controllers/searchResults.js
@@ -8,10 +8,6 @@
  * Controller of the smartCityFrontEndApp
  */
 angular.module('smartCityFrontEndApp')
-  .controller('SearchResultsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SearchResultsCtrl', function ($scope, serviceAjax, searchData) {
+    $scope.searchData = searchData;
   });
--- Frontend/app/scripts/services/serviceajax.js
+++ Frontend/app/scripts/services/serviceajax.js
@@ -12,25 +12,25 @@ angular.module('smartCityFrontEndApp')
     return{
         profils: function(successFct){
             //$http.get("../Server/app.php?a=criterias").success(successFct);
-            successFct([{name : 'Etudiant', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 3}, 
-                                                                                    {name : 'Transport commun', coef : 8}, 
-                                                                                    {name : 'Ecole', coef : 5}]},
+            successFct([{name : 'Etudiant', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 1}, 
+                                                                                    {name : 'Transport commun', coef : 2}, 
+                                                                                    {name : 'Ecole', coef : 3}]},
                         {name : 'Senior', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 3}, 
-                                                                                    {name : 'Transport commun', coef : 8}, 
-                                                                                    {name : 'Ecole', coef : 5}]},
-                        {name : 'Famille', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 3}, 
-                                                                                    {name : 'Transport commun', coef : 8}, 
-                                                                                    {name : 'Ecole', coef : 5}]},
-                        {name : 'Jeune Actif', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 3}, 
-                                                                                    {name : 'Transport commun', coef : 8}, 
-                                                                                    {name : 'Ecole', coef : 5}]},
+                                                                                    {name : 'Transport commun', coef : 2}, 
+                                                                                    {name : 'Ecole', coef : 1}]},
+                        {name : 'Famille', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 2}, 
+                                                                                    {name : 'Transport commun', coef : 3}, 
+                                                                                    {name : 'Ecole', coef : 1}]},
+                        {name : 'Jeune Actif', imgPath : 'images/yeoman.png', coefs : [{name : 'Bruit', coef : 2}, 
+                                                                                    {name : 'Transport commun', coef : 1}, 
+                                                                                    {name : 'Ecole', coef : 3}]},
                        ]);
         },
         criterias: function(successFct){
             //$http.get("../Server/app.php?a=profils").success(successFct);
-            successFct([{name : 'Bruit'},
-                        {name : 'Transport commun'},
-                        {name : 'Ecole'}]);
+            successFct([{name : 'Bruit', min : 0,max : 5},
+                        {name : 'Transport commun', min : 0,max : 8},
+                        {name : 'Ecole', min : 0,max : 3}]);
         },
         latlon: function(q, city, successFct){
             var url = 'http://nominatim.openstreetmap.org/search.php?q=' + encodeURI(q) + '&city=' + encodeURI(city) + '&state= France&format=json';
