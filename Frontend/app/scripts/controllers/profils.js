8f9ae6479b7bc7d15d3ee0b0bec1fee4da2fffd0
Rémy de Leydet <remy.deleydet@gmail.com>
2016-04-26 16:40:59 +0200

    Frontend - Ajout des sliders (lib + binding)

--- Frontend/app/scripts/controllers/profils.js
+++ Frontend/app/scripts/controllers/profils.js
@@ -8,25 +8,23 @@
  * Controller of the smartCityFrontEndApp
  */
  angular.module('smartCityFrontEndApp')
-  .controller('ProfilsCtrl', function ($scope, serviceAjax) {
-    
-    var loadProfils = function (){
+  .controller('ProfilsCtrl', function ($scope, serviceAjax, searchData) {
+    $scope.searchData = searchData;
+    $scope.show = false;
+    var loadProfilsAndCriterias = function (){
       serviceAjax.profils(function(data){
          $scope.profils = data; 
       });
+      serviceAjax.criterias(function(data){
+         for (var i = 0 ; i < data.length ; ++i){
+             searchData.criterias.push({name : data[i].name, coef : 5}); 
+        }
+      });
     };    
-    loadProfils();
-     
-  /* var slider = new Slider('#coef', {tooltip: 'always'}); */
-        
-    $scope.show = false;
+             
     $scope.loadDetails = function (){
         $scope.show = true;
-        serviceAjax.criterias(function(data){
-            $scope.criterias = data; 
-        });
-       // slider();
     }; 
-    
-    
+     
+    loadProfilsAndCriterias();
   });
--- Frontend/test/karma.conf.js
+++ Frontend/test/karma.conf.js
@@ -30,6 +30,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js',
+      'bower_components/angular-bootstrap-slider/slider.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
