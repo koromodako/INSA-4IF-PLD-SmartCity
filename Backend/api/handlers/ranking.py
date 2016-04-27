e1939784875f70be62751970a0d760750bb4955e
Gazolik <Gazolike@gmail.com>
2016-04-27 11:01:24 +0200

    add ranking

--- Backend/api/handlers/ranking.py
+++ Backend/api/handlers/ranking.py
@@ -1,18 +1,22 @@
 from ..py_rest.pyrest.rest_server.rest_api.response import Response
-from ..criteria.ranking import rank
+from ..criteria.gen_criteria import rank
+import json
 
 def ranking_handler(path, data, api_param):
+    print(json.loads(data['data'][0]))
     #extraire les criteres utiles
-    criteres= data['criteres']
+    criteres= json.loads(data['data'][0])['criteres']
+    print(criteres)
     nomcriteres=[c['name'] for c in criteres if c['coef'] !=0]
 
     #evaluer les criteres
     notes = {}
     for i in nomcriteres:
-        notes[i] = rank({'criteria':criterias_dict[i],coordinates:{'lat':data['lat'],'lon':data['lon']}})
+        notes[i] = 5#rank({'criteria':criterias_dict[i],coordinates:{'lat':data['lat'],'lon':data['lon']}})
     #faire une moyenne
     moy = sum(notes.values())/len(nomcriteres)
 
     #retourner les notes
     ret_data = {"moyenne":moy,"notes":notes}
+    print(json.dumps(ret_data))
     return Response(api_param).serialized(ret_data)
