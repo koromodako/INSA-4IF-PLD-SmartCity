7a23e5aebc5164e68c64bfc755fd69e7c22707b0
crihouey <cindy.rihouey@insa-lyon.fr>
2016-04-28 19:29:40 +0200

    design

--- Frontend/app/scripts/controllers/profils.js
+++ Frontend/app/scripts/controllers/profils.js
@@ -10,16 +10,17 @@
  angular.module('smartCityFrontEndApp')
   .controller('ProfilsCtrl', function ($scope, serviceAjax, searchData) {
      
-    $scope.searchData = searchData;
-    $scope.show = false;
-    $scope.msgCriteres = 'Afficher les critères';
-    $scope.profilSelected = null;
+    $scope.searchData = searchData;    
     $scope.customProfil = {name : 'Profil Perso', imgPath : 'yeoman.png', coefs : []};
      
     var loadProfils = function (){
       serviceAjax.profils(function(data){
           $scope.profils = data;
           $scope.profils.push($scope.customProfil);
+          $scope.profilSelected = $scope.customProfil;
+          $scope.show = true;
+          $scope.msgCriteres ='Cacher les critères';
+          
       });
     };
     loadProfils();
@@ -29,7 +30,7 @@
          searchData.criterias.length = 0;
          for (var i = 0 ; i < data.length ; ++i){
              $scope.customProfil.coefs.push({name : data[i].name, coef : 5});
-             searchData.criterias.push({name : data[i].name, coef : 5, code : data[i].code});          
+             searchData.criterias.push({name : data[i].name, coef : $scope.customProfil.coefs[i].coef, code : data[i].code});          
          }
       });
     };          
@@ -47,15 +48,16 @@
         }
     };
      
-    $scope.updateCoef = function (index){
+    $scope.updateCoef = function (profil){
         for (var i = 0 ; i < searchData.criterias.length ; ++i){
-            searchData.criterias[i].coef = $scope.profils[index].coefs[i].coef;        
+            searchData.criterias[i].coef = profil.coefs[i].coef;        
         }
-        $scope.profilSelected = index;  
-        if (index === $scope.profils.length - 1){
+        $scope.profilSelected = profil;
+        if ($scope.profilSelected === $scope.customProfil){
             $scope.show = true;
             $scope.msgCriteres ='Cacher les critères';
-        }
-    };     
+          }
+    }; 
+      
          
   });
