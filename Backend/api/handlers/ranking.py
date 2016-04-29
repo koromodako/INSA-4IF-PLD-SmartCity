1d0c4175b138af53d67df25456d63d49b2905d60
unknown <jwimsingue@IF501-204-12.insa-lyon.fr>
2016-04-29 12:05:06 +0200

    Envoi des détails avec la note, envoi du type lors de l'envoi des criteres

--- Backend/api/handlers/ranking.py
+++ Backend/api/handlers/ranking.py
@@ -14,9 +14,9 @@ def ranking_handler(path, data, api_param):
     somme = 0
     for i in nomcriteres:
         spec = {'criteria':criterias_dict[i],'coordinates':{'lat':d['lat'],'lon':d['lon']}}
-        notes[i],e = rank(spec)
+        notes[i],closest,density = rank(spec)
         somme = somme + notes[i]*criteres[i]
-        ret_note.append({"name":criterias_dict[i]['realname'],"note":notes[i]})
+        ret_note.append({"name":criterias_dict[i]['realname'],"note":notes[i], "closest" : closest, "density" : density})
     #faire une moyenne
     if sum(criteres.values())!=0:
         moy = somme/sum(criteres.values())
