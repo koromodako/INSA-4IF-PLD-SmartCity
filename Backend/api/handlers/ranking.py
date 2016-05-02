fcadc6c148520552022ace9afc722b87af28ebee
koromodako <koromodako@gmail.com>
2016-05-02 21:05:49 +0200

    retour du rayon pour la densité

--- Backend/api/handlers/ranking.py
+++ Backend/api/handlers/ranking.py
@@ -19,15 +19,19 @@ def ranking_handler(path, data, api_param):
     for i in criteres.keys():
         spec = {'criteria': criterias_dict[i], 'coordinates': {'lat': d['lat'], 'lon': d['lon']}}
         note, closest, density = rank(spec)
+        # récupération du rayon dans le cas d'une densité
+        # on retourne explicitement None si on ne trouve pas la clé radius dans params
+        radius = criterias_dict[i]['params'].get('radius', None) 
+        # traitement en fonction du coeff du critère
         if criteres[i] == 0 :
-            ret_note.append({'name': criterias_dict[i]['realname'], 'note':note, 'satisfaction':-101,'closest': closest, 'density': None})
+            ret_note.append({'name': criterias_dict[i]['realname'], 'note':note, 'satisfaction':-101,'closest': closest, 'density': density, 'radius': radius})
         else :
             notes[i] = note
             somme = somme + notes[i] * criteres[i]
             note_finale = notes[i]
             #calcul de satisfaction
             satis = satisfaction(max(note_finale,0), criteres[i])
-            ret_note.append({'name': criterias_dict[i]['realname'], 'note':round(note_finale, 2), 'satisfaction':round(satis, 2), 'closest': closest, 'density': density})
+            ret_note.append({'name': criterias_dict[i]['realname'], 'note':round(note_finale, 2), 'satisfaction':round(satis, 2), 'closest': closest, 'density': density, 'radius': radius})
     # faire une moyenne
     if sum(criteres.values()) != 0:
         moy = somme / sum(criteres.values())
