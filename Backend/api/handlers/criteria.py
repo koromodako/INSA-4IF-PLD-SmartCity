71622c3a018c17f284249fee8f41b9015df72157
Gazolik <Gazolike@gmail.com>
2016-05-03 08:23:34 +0200

    ajout dist et dens dans criteres

--- Backend/api/handlers/criteria.py
+++ Backend/api/handlers/criteria.py
@@ -10,5 +10,26 @@ def criterias_handler(path, data, api_params):
         TODO : doc
     """
     criteres = load_static('criteres')
-    name_list = [{'code': k, 'name': criteres[k]['realname'], 'groupe': criteres[k]['group'], 'description': criteres[k]['description'], 'type': criteres[k]['type']} for k in criteres.keys()]
+    name_list = []
+    for k in criteres.keys():
+        criteria = {'code': k,
+                'name': criteres[k]['realname'],
+                'groupe': criteres[k]['group'],
+                'description': criteres[k]['description'],
+                'type': criteres[k]['type'] }
+        if criteres[k]['type'] == 'distance_based':
+            criteria['dist'] = [criteres[k]['params']['min_dist'],criteres[k]['params']['max_dist']]
+            criteria['dens'] = []
+        elif criteres[k]['type'] == 'density_based':
+            criteria['dist'] = []
+            criteria['dens']=[criteres[k]['params']['min_density'],criteres[k]['params']['max_density']]
+
+        elif criteres[k]['type'] == 'dist_dens_based':
+            criteria['dist'] = [criteres[k]['params']['min_dist'],criteres[k]['params']['max_dist']]
+            criteria['dens'] = [criteres[k]['params']['min_density'],criteres[k]['params']['max_density']]
+
+        else:
+            criteres[k]['dist'] = []
+            criteres[k]['dens'] = []
+        name_list.append(criteria)
     return Response(api_params).serialized({'criteres': name_list})
